e132b8ae064eb85a5b76d8b5fa64dc0750a98eb1
jesquinh <jesquinh@emeal.nttdata.com>
2025-12-11 12:46:02 +0100

    Defined TestInstance to refresh in every Test

--- src/test/java/scripts/MarcaTest.java
+++ src/test/java/scripts/MarcaTest.java
@@ -2,11 +2,13 @@ package scripts;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import pages.Marca;
 
 import static org.hamcrest.Matchers.is;
 import static utilities.FrontEndOperation.checkThat;
 
+@TestInstance(TestInstance.Lifecycle.PER_METHOD)
 public class MarcaTest extends GenericTest<Marca> {
 
     @BeforeEach
