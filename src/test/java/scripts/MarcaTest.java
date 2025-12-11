7fb6de750d52a1e4e61f2395cf7d5b4aac8196f0
jesquinh <jesquinh@emeal.nttdata.com>
2025-12-11 12:46:52 +0100

    Joined tests to "check if error message is visible" and "check if error message is expected" into one

--- src/test/java/scripts/MarcaTest.java
+++ src/test/java/scripts/MarcaTest.java
@@ -19,13 +19,7 @@ public class MarcaTest extends GenericTest<Marca> {
     @Test
     public void checkLoginErrorMessage() {
         controller.fillLogin();
-        checkThat("Comparing visible message", controller.isVisible(), is(true));
-    }
-
-    @Test
-    public void checkErrorMessage() {
-        controller.acceptCookies();
-        controller.fillLogin();
+        checkThat("Error message is visible", controller.isErrorMessageVisible(), is(true));
         controller.compareVisibleMessage();
     }
 }
